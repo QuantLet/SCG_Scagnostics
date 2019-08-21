5ff161d83564a42ff9fce2c8998464c4c68e8587
Ioana Ceausu <41063111+ceausuio@users.noreply.github.com>
2019-08-21 15:24:59 +0300

    Update SCGDTscatterplot.R

--- SCGdt/SCGDTscatterplot.R
+++ SCGdt/SCGDTscatterplot.R
@@ -3,7 +3,7 @@ library(tripack)
 library(MASS)
 
 par(bg = NA)
-plot(Boston$ptratio, Boston$tax, pch = 19, col = "blue", xlab = "zn", ylab = "dis",
+plot(Boston$ptratio, Boston$tax, pch = 19, col = "blue", xlab = "pt rario", ylab = "tax",
      main = "ZN - DIS Scatterplot")
 
 tdf = tri.mesh(Boston$ptratio, Boston$tax, duplicate = "remove")
