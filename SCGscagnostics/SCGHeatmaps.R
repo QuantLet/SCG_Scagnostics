d83f9a8527ba3173cc7ecd6a8a05543dacf46940
Ioana Ceausu <41063111+ceausuio@users.noreply.github.com>
2019-08-21 15:23:33 +0300

    Update SCGHeatmaps.R

--- SCGscagnostics/SCGHeatmaps.R
+++ SCGscagnostics/SCGHeatmaps.R
@@ -33,12 +33,3 @@ s = scagnostics(factors_scag_gr)
 s = as.matrix(s)
 
 
-#  Heatmap of PC Factors --------------------------------------------------------
-
-png(filename = "Heatmap PC Factors.png", bg = NA)
-hm <- heatmap.2(s, Rowv=NA, Colv=NA,
-                dendrogram = "none",  ## to suppress warnings
-                margins=c(5,5), cexRow=0.5, cexCol=1.0, key=TRUE, keysize=1.5,
-                trace="none", scale(s1))
-
-dev.off()
